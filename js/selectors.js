6782b6c8d95988f4481f25bb339f1de53191ae17
Marina Paskal <paskalm@rogers.com>
2017-05-18 05:48:01 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  var paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector(){
+  var last = $('img:last');
+  return last;
+}
+
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector(){
+  var div = $('.pics');
+  return div;
+}
+
+function firstListItem(){
+  var first = $('ul li:first-child');
+  return first;
+}
