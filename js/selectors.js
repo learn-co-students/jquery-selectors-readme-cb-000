1e4d1d6e9e691b174cb78044239e0ae3bf015542
cptbrkfst <dyl@resonategroup.ca>
2017-12-10 15:03:42 -0500

    completed lab

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+var paragraphSelector = () => {
+  return $('p')
+}
+
+var lastImageSelector = () => {
+  return $('img:last')
+}
+
+var ninjaBabySelector = () => {
+  return $('#baby-ninja')
+}
+
+var divSelector = () => {
+  return $('div.pics')
+}
+
+var firstListItem = () => {
+  return $('li:first-child')
+}
